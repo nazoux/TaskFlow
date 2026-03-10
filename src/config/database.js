c0cdc00fe1e1dbbac61a4b56ba7abedc381f0b98
nazoux <darkevent774@gmail.com>
2026-03-10 15:32:31 +0100

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: 28934,
     dialect: "mysql",
     logging: false
   }
